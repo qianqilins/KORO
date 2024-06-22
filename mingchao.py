35e7726bb7d8f9271d0da6c485f7c6992989e023
qianqilins <52474322+qianqilins@users.noreply.github.com>
2024-06-23 05:49:06 +0800

    Update mingchao.py

--- mingchao.py
+++ mingchao.py
@@ -1,6 +1,7 @@
-from keys import token
 from log import mc_message
-import requests, json, datetime, sys, re, time, os
+import requests, json, datetime, os, sys, re, time
+
+
 
 # 获取鸣潮表单
 def mingchaoTable(token):
