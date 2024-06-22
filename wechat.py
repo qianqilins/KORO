a68810be5f320c0ec4d8eea46ae54aae5af302dc
qianqilins <52474322+qianqilins@users.noreply.github.com>
2024-06-23 06:24:20 +0800

    Create wechat.py

---
+++ wechat.py
@@ -0,0 +1,15 @@
+import requests
+
+WEAPI = 
+
+def wemessage(WEAPI)
+url = f"https://api.anpush.com/push/{WEAPI}"
+payload = {
+    "title": "鸣潮",
+    "content": "your_content",
+    "channel": "微信通知"
+}
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+response = requests.post(url, headers=headers, data=payload)
