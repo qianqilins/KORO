1d6f4e783f7162c1702a9a183ff0e000c4a3b86c
qianqilins <52474322+qianqilins@users.noreply.github.com>
2024-06-23 06:31:53 +0800

    Update wechat.py

--- wechat.py
+++ wechat.py
@@ -3,13 +3,13 @@ import requests
 WEAPI = os.getenv('WEAPI')
 
 def wemessage(WEAPI, CONTENT)
-url = f"https://api.anpush.com/push/{WEAPI}"
-payload = {
-    "title": "鸣潮",
-    "channel": "微信通知",
-    "content": CONTENT
-}
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-response = requests.post(url, headers=headers, data=payload)
+    url = f"https://api.anpush.com/push/{WEAPI}"
+    payload = {
+        "title": "鸣潮",
+        "channel": "微信通知",
+        "content": CONTENT
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    response = requests.post(url, headers=headers, data=payload)
