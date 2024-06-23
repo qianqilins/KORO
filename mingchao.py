b56a38cff7620740b85b077554c872943d9ad380
qianqilins <52474322+qianqilins@users.noreply.github.com>
2024-06-23 08:03:22 +0800

    Update mingchao.py

--- mingchao.py
+++ mingchao.py
@@ -176,5 +176,5 @@ mingchaoIndexWrite(token, mc.roleId, mc.userId)
 if mc.signInTxt != '已完成签到':
     mingchaoSignin(token, mc.roleId, mc.userId, mc.month)
 
-with open(mc_log, 'r', encoding='GBK') as CONTENT:
+with open(mc_log, 'r', encoding='gbk', errors='ignore') as CONTENT:
     wemessage(WEAPI, WEUSERID, CONTENT.read().replace('\n', '<br>'))
