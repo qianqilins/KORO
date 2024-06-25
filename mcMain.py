e1abe7d092e714d5b3ab155cd88346dd4bbcf049
qianqilins <52474322+qianqilins@users.noreply.github.com>
2024-06-26 07:05:33 +0800

    Update mcMain.py

--- mcMain.py
+++ mcMain.py
@@ -32,7 +32,7 @@ mcBaseWrite(token, mc.roleId, mc.userId)
 mcIndexWrite(token, mc.roleId, mc.userId)
 
 # 云端不做判断
-mingchaoSignin(token, mc.roleId, mc.userId)
+print(mingchaoSignin(token, mc.roleId, mc.userId))
 # if mc.signInTxt != '已完成签到':
 #     print('获得补给：'+mingchaoSignin(token, mc.roleId, mc.userId))
 
