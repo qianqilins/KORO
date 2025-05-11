994da14691047c9f9007476061612b0573b3d6d5
qianqilins <52474322+qianqilins@users.noreply.github.com>
2025-05-12 03:56:16 +0800

    Update zsUtil.py

--- zsUtil.py
+++ zsUtil.py
@@ -39,7 +39,7 @@ def zhansuangRefresh(token, devCode):
 def serverSwich(token, devCode):
     url = "http://api.kurobbs.com/config/dict/timingSwitch"
     headers = {
-        "version": "2.2.0",
+        "version": "2.4.4",
         "devCode": devCode,
         "source": "android",
         "token": token,
